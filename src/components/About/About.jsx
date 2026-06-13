f396e13818ae4619f0ce556c568961600cc4c88b
CodyMarsee <cmarsee@alumni.iu.edu>
2026-06-12 21:01:15 -0400

    Post GOH

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,10 +2,11 @@ import "./About.css";
 
 function About() {
   return (
-    <>
+    <about className="about">
+      <div className="about__author-image"></div>
       <div className="about__title">About the Author</div>
       <p className="about__description"></p>
-    </>
+    </about>
   );
 }
 
